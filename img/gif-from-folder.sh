003845f905246f05a745525fcad2744309e2b492
Samuel Leong <leowhiz@yahoo.com.sg>
2022-04-14 15:30:55 -0400

    change way video is saved

--- img/gif-from-folder.sh
+++ img/gif-from-folder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT_NAME=$1
+OUTPUT_NAME=${1%/}
 
 if [ -z $OUTPUT_NAME ]; then
     echo "USAGE: ./gif-from-folder.sh <subfolder-output-name> [frame-rate]"
@@ -10,4 +10,4 @@ fi
 # RESULTS=${2:-results}
 FRAME_RATE=${2:-10}
 
-ffmpeg -y -start_number 1 -framerate $FRAME_RATE -i $OUTPUT_NAME/%0004d.jpg -loop -1 $OUTPUT_NAME/result.gif
+ffmpeg -y -start_number 1 -framerate $FRAME_RATE -i $OUTPUT_NAME/%0004d.jpg -loop -1 $OUTPUT_NAME.gif
--- img/mp4-from-folder.sh
+++ img/mp4-from-folder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT_NAME=$1
+OUTPUT_NAME=${1%/}
 
 if [ -z $OUTPUT_NAME ]; then
     echo "USAGE: ./mp4-from-folder.sh <folder-name> [frame-rate]"
@@ -9,4 +9,4 @@ fi
 
 FRAME_RATE=${2:-10}
 
-ffmpeg -y -start_number 1 -framerate $FRAME_RATE -i $OUTPUT_NAME/%0004d.jpg -loop -1 -profile:v high -crf 28 -pix_fmt yuv420p $OUTPUT_NAME/result.mp4
+ffmpeg -y -start_number 1 -framerate $FRAME_RATE -i $OUTPUT_NAME/%0004d.jpg -loop -1 -profile:v high -crf 28 -pix_fmt yuv420p $OUTPUT_NAME.mp4
