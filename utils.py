38e992aad21dd8ff63674439afc3a44a8089e507
Samuel Leong <leowhiz@yahoo.com.sg>
2022-04-22 12:49:25 -0400

    added function to save by subplot

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
 import numpy as np
-import math
 import time
 import os
-import numpy as np
+
 from matplotlib import pyplot as plt
+from matplotlib.transforms import Bbox
+
 
 def tic():
     return time.time()
 
+
 def toc(tic):
     return time.time() - tic
 
+
 def f_arr(xs):
     return '[' + ','.join([f'{x:03f}' for x in xs]) + ']'
+
 
 def radarImgPathToTimestamp(radarImgPath):
     """
@@ -20,11 +24,13 @@ def radarImgPathToTimestamp(radarImgPath):
     """
     return int(os.path.basename(radarImgPath)[:-4])
 
+
 def normalize_angles(th):
     """
     Normalize an angle to be between -pi and pi
     """
     return (th + np.pi) % (2 * np.pi) - np.pi
+
 
 def getRotationMatrix(th, degrees=False):
     if degrees:
@@ -34,6 +40,7 @@ def getRotationMatrix(th, degrees=False):
     R = np.array([[cth, -sth], [sth, cth]])
 
     return R
+
 
 def convertPoseToTransform(poses):
     '''
@@ -46,22 +53,23 @@ def convertPoseToTransform(poses):
     if len(poses.shape) == 1:
         single = True
         poses = np.expand_dims(poses, axis=0)
-    xs = poses[:,0]
-    ys = poses[:,1]
-    ths = poses[:,2]
+    xs = poses[:, 0]
+    ys = poses[:, 1]
+    ths = poses[:, 2]
     cths = np.cos(ths)
     sths = np.sin(ths)
-    pose_transform = np.zeros((len(ths),3,3))
-    pose_transform[:,0,0] = cths
-    pose_transform[:,0,1] = -sths
-    pose_transform[:,1,0] = sths
-    pose_transform[:,1,1] = cths
-    pose_transform[:,0,2] = xs
-    pose_transform[:,1,2] = ys
-    pose_transform[:,2,2] = 1
+    pose_transform = np.zeros((len(ths), 3, 3))
+    pose_transform[:, 0, 0] = cths
+    pose_transform[:, 0, 1] = -sths
+    pose_transform[:, 1, 0] = sths
+    pose_transform[:, 1, 1] = cths
+    pose_transform[:, 0, 2] = xs
+    pose_transform[:, 1, 2] = ys
+    pose_transform[:, 2, 2] = 1
     if single:
-        pose_transform = pose_transform[0,:,:]
+        pose_transform = pose_transform[0, :, :]
     return pose_transform
+
 
 def convertTransformToPose(pose_transforms):
     '''
@@ -74,22 +82,25 @@ def convertTransformToPose(pose_transforms):
     if len(pose_transforms.shape) == 2:
         single = True
         pose_transforms = np.expand_dims(pose_transforms, axis=0)
-    ths = np.arctan2(pose_transforms[:,1,0], pose_transforms[:,0,0])
-    xs = pose_transforms[:,0,2]
-    ys = pose_transforms[:,1,2]
+    ths = np.arctan2(pose_transforms[:, 1, 0], pose_transforms[:, 0, 0])
+    xs = pose_transforms[:, 0, 2]
+    ys = pose_transforms[:, 1, 2]
     pose_transforms = np.stack([xs, ys, ths], axis=1)
     if single:
         pose_transforms = pose_transforms[0, :]
     return pose_transforms
 
+
 def flatten(x):
     return x.reshape(x.shape[0])
+
 
 def convertRandHtoDeltas(R, h):
     dx = float(*h[0])
     dy = float(*h[1])
-    dth = np.arctan2(R[1,0], R[0,0])
+    dth = np.arctan2(R[1, 0], R[0, 0])
     return np.array([dx, dy, dth])
+
 
 def quiver(poses, c='r'):
     poses = np.array(poses)
@@ -103,3 +114,29 @@ def quiver(poses, c='r'):
         scale=10,
         alpha=.5,
     )
+
+
+def plt_full_extent(ax, pad=0.0):
+    """
+    @brief Get the full extent of a plt axes, including axes labels, tick labels, and titles.
+    """
+    # For text objects, we need to draw the figure first, otherwise the extents
+    # are undefined.
+    ax.figure.canvas.draw()
+    items = ax.get_xticklabels() + ax.get_yticklabels()
+    items += [ax, ax.title, ax.xaxis.label, ax.yaxis.label]
+
+    bbox = Bbox.union([item.get_window_extent() for item in items])
+
+    return bbox.expanded(1.0 + pad, 1.0 + pad)
+
+
+def plt_savefig_by_axis(filePath, fig, ax, pad=0.0):
+    '''
+    @brief Save a plt figure by extent of its axis (allows us to save subplots)
+    @param[in] filePath Path to save figure to
+    @param[in] fig Overall figure containing axis
+    @param[in] ax Axis to save
+    '''
+    extent = plt_full_extent(ax, pad).transformed(fig.dpi_scale_trans.inverted())
+    fig.savefig(filePath, bbox_inches=extent)
