minor fixes with radians vs degrees mixup

RawROAMSystem.py
@@ -143,8 +143,9 @@ class RawROAMSystem():
         timestamp = radarImgPathToTimestamp(imgPathArr[seqInd])
         est_deltas = convertRandHtoDeltas(R, h)
         dx = est_deltas[0]
+        dy = est_deltas[1]
         dth = est_deltas[2]
-        self.estTraj.appendRelativeDxDth(timestamp, dx, dth)
+        self.estTraj.appendRelativeDeltas(timestamp, [dx, dy, dth])
         # self.estTraj.appendRelativeTransform(timestamp, R, h)
 
     def plot(self, prevImg, currImg, good_old, good_new, R, h, seqInd):
@@ -167,22 +168,22 @@ class RawROAMSystem():
 
         # Get timestamps for plotting etc
         currTimestamp = radarImgPathToTimestamp(imgPathArr[seqInd])
-        gt_deltas = gtTraj.getGroundTruthDeltasAtTime(currTimestamp)
-        gt_deltas[2] = np.rad2deg(gt_deltas[2])
+        gt_deltas_deg = gtTraj.getGroundTruthDeltasAtTime(currTimestamp)
+        gt_deltas_deg[2] = np.rad2deg(gt_deltas_deg[2])
 
-        est_deltas = convertRandHtoDeltas(R, h)
-        est_deltas[2] = np.rad2deg(est_deltas[2])
+        est_deltas_deg = convertRandHtoDeltas(R, h)
+        est_deltas_deg[2] = np.rad2deg(est_deltas_deg[2])
 
-        print(f"GT Deltas: {f_arr(gt_deltas)}")
-        print(f"Est Deltas: {f_arr(est_deltas)} (*dth in degrees)")
+        print(f"GT Deltas: {f_arr(gt_deltas_deg)} (*dth in degrees)")
+        print(f"Est Deltas: {f_arr(est_deltas_deg)} (*dth in degrees)")
 
         # Plot Trajectories
         toSaveTrajPath = os.path.join(trajSavePath, f"{seqInd:04d}.jpg")
         plotGtAndEstTrajectory(gtTraj,
                                estTraj, f'[{seqInd}]\n'
                                f'Est Pose: {f_arr(estTraj.poses[-1])}\n'
-                               f'GT Deltas: {f_arr(gt_deltas)}\n'
-                               f'Est Deltas: {f_arr(est_deltas)}\n',
+                               f'GT Deltas: {f_arr(gt_deltas_deg)}\n'
+                               f'Est Deltas: {f_arr(est_deltas_deg)}\n',
                                savePath=toSaveTrajPath)
 
 

trajectoryPlotting.py
@@ -25,11 +25,12 @@ class Trajectory():
         '''
         return self.gt_deltas[time]
 
-    def appendRelativeDxDth(self, time, dx, dth):
+    def appendRelativeDeltas(self, time, d_xyth):
+        dx, dy, dth = d_xyth
         self.timestamps = np.append(self.timestamps, time)
         x, y, th = self.poses[-1]
-        x += dx * np.cos(th)
-        y += dx * np.sin(th)
+        x += dx * np.cos(th) - dy * np.sin(th)
+        y += dx * np.sin(th) + dy * np.cos(th)
         th += dth
         self.poses = np.vstack((self.poses, [x, y, th]))
 