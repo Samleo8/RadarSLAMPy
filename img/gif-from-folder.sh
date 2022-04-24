5fc1e69f28a8571454cbb7be757ac7c8ab9dd36c
Samuel Leong <leowhiz@yahoo.com.sg>
2022-04-23 20:38:36 -0400

    Allow .sh files to have seqInd starting numbers

--- img/gif-from-folder.sh
+++ img/gif-from-folder.sh
@@ -3,11 +3,11 @@
 OUTPUT_NAME=${1%/}
 
 if [ -z $OUTPUT_NAME ]; then
-    echo "USAGE: ./gif-from-folder.sh <subfolder-output-name> [frame-rate]"
+    echo "USAGE: ./gif-from-folder.sh <subfolder-output-name> [start_number] [frame-rate]"
     exit
 fi
 
-# RESULTS=${2:-results}
-FRAME_RATE=${2:-10}
+START_NUM=${2:-1}
+FRAME_RATE=${3:-10}
 
-ffmpeg -y -start_number 1 -framerate $FRAME_RATE -i $OUTPUT_NAME/%0004d.jpg -loop -1 $OUTPUT_NAME.gif
+ffmpeg -y -start_number $START_NUM -framerate $FRAME_RATE -i $OUTPUT_NAME/%0004d.jpg -loop -1 $OUTPUT_NAME.gif
