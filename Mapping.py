80cbce1194d113c5ef26ef7f5750229c8ba0db58
Samuel Leong <leowhiz@yahoo.com.sg>
2022-04-29 03:52:17 -0400

    added keyframe goodness check

--- Mapping.py
+++ Mapping.py
@@ -1,6 +1,29 @@
 import numpy as np
-from trajectoryPlotting import Trajectory
+import scipy as sp
 
+from trajectoryPlotting import Trajectory
+import m2dp
+from getPointCloud import getPointCloudPolarInd
+
+# Thresholds
+ROT_THRESHOLD = 0.2 # radians
+TRANS_THRESHOLD = 2.0 # meters
+TRANS_THRESHOLD_SQ = TRANS_THRESHOLD * TRANS_THRESHOLD # meters^2
+
+# Keyframe class
+class Keyframe():
+
+    def __init__(self, pose: np.ndarray, featurePoints: np.ndarray,
+                 radarPolarImg: np.ndarray) -> None:
+        self.pose = pose
+        self.featurePoints = featurePoints  # set of (tracked) feature points
+        self.radarPolarImg = radarPolarImg  # radar polar image
+
+        # TODO: Not sure if needed/useful
+        self.pointCloud = getPointCloudPolarInd(radarPolarImg)
+
+
+# Map class
 class Map():
 
     def __init__(self, sequenceName: str, estTraj: Trajectory,
@@ -13,9 +36,33 @@ class Map():
         self.filePaths = filePaths
 
         self.estTraj = estTraj
+        self.keyframes = []
 
-    def isGoodKeyframe(self, frame, R, h):
-        pass
+    # TODO: might not want to make keyframe before adding it
+    def isGoodKeyframe(self, keyframe: Keyframe):
+        # Get information of prev KF's pose
+        prevKF = self.keyframes[-1]
+        srcPose = prevKF.pose
 
-    def addKeyframe(self, frame, R, h):
+        # Get the information of possible KF's pose
+        targetPose = keyframe.pose
+
+        # Check rotation condition relative to last keyframe
+        # TODO: Check if radians or degrees?
+        deltaTh = np.abs(srcPose[2] - targetPose[2])
+
+        if (deltaTh >= ROT_THRESHOLD):
+            return True
+
+        # Check translation condition
+        deltaTrans = (srcPose[0:2] - targetPose[0:2]) ** 2
+        deltaTrans = deltaTrans.sum()
+
+        if (deltaTrans >= TRANS_THRESHOLD_SQ):
+            return True
+
+        return False
+
+    def addKeyframe(self, keyframe: Keyframe):
+        self.keyframes.append(keyframe)
         pass
